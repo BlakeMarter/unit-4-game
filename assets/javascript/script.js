625f4eefabbf9e1bff91118feaf43be9ea7ae2c7
Legionnaire <blake.marter@gmail.com>
2019-03-20 23:14:20 -0700

    Final touchups

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,61 +3,80 @@ $(document).ready(function () {
 
 
 // Setting the random value for number goal.
-	var compPick = Math.floor(Math.random() * 102) + 19;
-	$(".randomNumber").html("Goal: " + compPick);
+	var compChoice = Math.floor(Math.random() * 120) + 19;
+	$(".numGoal").html("Goal: " + compChoice);
 
 
 // Setting the random values for each crystal.
 	var gemOne = Math.floor(Math.random() * 12) + 1;
-	$("#img1").html("<img src=" + "assets/images/image1.1.png" + " value=" + gemOne + ">");
+	$("#img1").html("<img src=" + "assets/images/img1.png" + " value=" + gemOne + ">");
 
 	var gemTwo = Math.floor(Math.random() * 12) + 1;
-	$("#img2").html("<img src=" + "assets/images/image2.1.png" + " value=" + gemTwo + ">");
+	$("#img2").html("<img src=" + "assets/images/img2.png" + " value=" + gemTwo + ">");
 
 	var gemThree = Math.floor(Math.random() * 12) + 1;
-	$("#img3").html("<img src=" + "assets/images/image3.1.png" + " value=" + gemThree + ">");
+	$("#img3").html("<img src=" + "assets/images/img3.png" + " value=" + gemThree + ">");
 
 	var gemFour = Math.floor(Math.random() * 12) + 1;
-	$("#img4").html("<img src=" + "assets/images/image4.1.png" + " value=" + gemFour + ">");
+	$("#img4").html("<img src=" + "assets/images/img4.png" + " value=" + gemFour + ">");
 
 	var wins = 0;
-
 	var losses = 0;
-
 	var score = 0;
+
+// Initializing game on load.
+$("img").on("click", function () {
+	var newScore = score += parseInt($(this).attr("value"));
+	$(".userScore").html(newScore);
+
+	if (newScore === compChoice) {
+		wins++;
+		$(".wins").html("Wins: " + wins);
+		alert("Congratulations! You Won!");
+		reset();
+	}
+
+	else if (newScore > compChoice) {
+		losses++;
+		$(".losses").html("Losses: " + losses);
+		alert("Womp Womp! You lost... :(");
+		reset();
+	}
+
+});
 
 // Resets entire game after win or loss. 
 	function reset() {
-		compPick = Math.floor(Math.random() * 102) + 19;
-		$(".randomNumber").html("Goal: " + compPick);
+		compChoice = Math.floor(Math.random() * 120) + 19;
+		$(".numGoal").html("Goal: " + compChoice);
 
 		score = 0;
-		$(".scoreDisplay").html(score);
+		$(".userScore").html(score);
 
 		gemOne = Math.floor(Math.random() * 12) + 1;
-		$("#img1").html("<img src=" + "assets/images/image1.1.png" + " value=" + gemOne + ">");
+		$("#img1").html("<img src=" + "assets/images/img1.png" + " value=" + gemOne + ">");
 
 		gemTwo = Math.floor(Math.random() * 12) + 1;
-		$("#img2").html("<img src=" + "assets/images/image2.1.png" + " value=" + gemTwo + ">");
+		$("#img2").html("<img src=" + "assets/images/img2.png" + " value=" + gemTwo + ">");
 
 		gemThree = Math.floor(Math.random() * 12) + 1;
-		$("#img3").html("<img src=" + "assets/images/image3.1.png" + " value=" + gemThree + ">");
+		$("#img3").html("<img src=" + "assets/images/img3.png" + " value=" + gemThree + ">");
 
 		gemFour = Math.floor(Math.random() * 12) + 1;
-		$("#img4").html("<img src=" + "assets/images/image4.1.png" + " value=" + gemFour + ">");
+		$("#img4").html("<img src=" + "assets/images/img4.png" + " value=" + gemFour + ">");
 
 		$("img").on("click", function () {
 			var newScore = score += parseInt($(this).attr("value"));
-			$(".scoreDisplay").html(newScore);
+			$(".userScore").html(newScore);
 
-			if (newScore === compPick) {
+			if (newScore === compChoice) {
 				wins++;
 				$(".wins").html("Wins: " + wins);
 				alert("Congratulations! You Won!");
 				reset();
 			}
 
-			else if (newScore > compPick) {
+			else if (newScore > compChoice) {
 				losses++;
 				$(".losses").html("Losses: " + losses);
 				alert("Womp Womp! You lost... :(");
@@ -65,25 +84,4 @@ $(document).ready(function () {
 			}
 		});
 	}
-// Initializing game on load.
-	$("img").on("click", function () {
-		var newScore = score += parseInt($(this).attr("value"));
-		$(".scoreDisplay").html(newScore);
-
-		if (newScore === compPick) {
-			wins++;
-			$(".wins").html("Wins: " + wins);
-			alert("Congratulations! You Won!");
-			reset();
-		}
-
-		else if (newScore > compPick) {
-			losses++;
-			$(".losses").html("Losses: " + losses);
-			alert("Womp Womp! You lost... :(");
-			reset();
-		}
-
-	});
-
 }); 
